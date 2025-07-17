18001a471ab3c96c0a77ce13d5382aa438915408
Maat94 <maatt94@gmail.com>
2025-07-16 23:29:43 -0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
       marca: 'Apple',
       modelo: 'iPhone 16 Pro Max 512 GB',
       precio: 2380000,
-      imagen: 'https://raw.githubusercontent.com/Maat94/proyecto-final-25020-gimenez/main/Images/Iphone-cart/iphone-16-plus.png',
+      imagen: 'https://raw.githubusercontent.com/Maat94/proyecto-final-25020-gimenez/main/Images/Images/Iphone-cart/iphone-16-plus.png',
     },
     {
       id: 2,
